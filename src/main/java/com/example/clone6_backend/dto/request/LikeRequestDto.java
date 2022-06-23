4b6119e676abd8812625500c85f73d37ff7daf3d
ssw2570 <sinsang2570@gmail.com>
2022-06-23 14:12:00 +0900

    like 실행이 안되서 주석 처리했습니다

--- src/main/java/com/example/clone6_backend/dto/request/LikeRequestDto.java
+++ src/main/java/com/example/clone6_backend/dto/request/LikeRequestDto.java
@@ -1,17 +1,17 @@
-package com.example.clone6_backend.dto.request;
-
-import com.example.clone6_backend.model.Fund;
-import com.example.clone6_backend.model.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class LikeRequestDto {
-    private User user;
-    private Fund fund;
-
-
-}
+//package com.example.clone6_backend.dto.request;
+//
+//import com.example.clone6_backend.model.Fund;
+//import com.example.clone6_backend.model.User;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//public class LikeRequestDto {
+//    private User user;
+//    private Fund fund;
+//
+//
+//}
